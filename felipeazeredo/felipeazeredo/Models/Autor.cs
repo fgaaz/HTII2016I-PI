2deb3c3abafa95016bd2270e32762a1a0426c3c6
fgaaz <felipe.fgaaz@hotmail.com>
2016-06-21 21:35:36 -0300

    Trabalho Final Pi - 21/06/16

---
+++ felipeazeredo/felipeazeredo/Models/Autor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace felipeazeredo.Models
+{
+    [Table("autores")]
+    public class Autor
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "O Nome deve ter entre 20 e 100 caracteres")]
+        [Required]
+        public string Nome { get; set; }
+
+        public List<Livro> Livros { get; set; }
+    }
+}
---
+++ felipeazeredo/felipeazeredo/Models/Genero.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace felipeazeredo.Models
+{
+    [Table("generos")]
+    public class Genero
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "A descrição deve ter entre 5 e 50 caracteres")]
+        [Required]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        public List<Livro> Livros { get; set; }
+    }
+}
